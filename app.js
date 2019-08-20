a17744363304a64de0f0dfd2ce1342afb17e0baa
imapolaris <sun_hui2011@sina.com>
2019-08-20 18:19:39 +0800

    添加页面

--- app.js
+++ app.js
@@ -32,6 +32,15 @@ App({
         }
       }
     })
+
+		wx.getSystemInfo({
+			success: e => {
+				this.globalData.StatusBar = e.statusBarHeight;
+				let custom = wx.getMenuButtonBoundingClientRect();
+				this.globalData.Custom = custom;
+				this.globalData.CustomBar = custom.bottom + custom.top - e.statusBarHeight;
+			}
+		})
   },
   globalData: {
     userInfo: null
